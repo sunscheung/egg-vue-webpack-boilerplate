7961bd1bedd39ec43534b759043bd149ac234561
sunscheung <sunscheung@163.com>
2019-11-27 20:17:41 +0800

    解决build时报错问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 module.exports = {
   egg: true,
-  framework: 'vue',
+  framework: "vue",
   entry: {
-    include: ['app/web/page'],
-    exclude: ['app/web/page/[a-z]+/component', 'app/web/page/test'],
+    include: ["app/web/page"],
+    exclude: ["app/web/page/[a-z]+/component", "app/web/page/test"],
     loader: {
-      client: 'app/web/framework/vue/entry/client-loader.js',
-      server: 'app/web/framework/vue/entry/server-loader.js',
+      client: "app/web/framework/vue/entry/client-loader.js",
+      server: "app/web/framework/vue/entry/server-loader.js"
     }
   },
   alias: {
-    server: 'app/web/framework/vue/entry/server.js',
-    client: 'app/web/framework/vue/entry/client.js',
-    asset: 'app/web/asset',
-    component: 'app/web/component',
-    framework: 'app/web/framework',
-    vue: 'vue/dist/vue.esm.js'
+    server: "app/web/framework/vue/entry/server.js",
+    client: "app/web/framework/vue/entry/client.js",
+    asset: "app/web/asset",
+    component: "app/web/component",
+    framework: "app/web/framework",
+    vue: "vue/dist/vue.esm.js"
   },
-  dll: ['vue', 'axios'],
+  dll: ["vue", "axios"],
   loaders: {},
-  plugins: {},
-  done() {
-
-  }
+  plugins: {
+    imagemini: false
+  },
+  done() {}
 };
